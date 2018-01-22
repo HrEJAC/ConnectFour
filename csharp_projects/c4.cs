3a5867c4c29c01e0a2f4950b63af8ebb2be84b7e
Elias Christensen <ejac@me.com>
2018-01-22 15:58:03 +0100

    new update

--- csharp_projects/c4.cs
+++ csharp_projects/c4.cs
@@ -9,6 +9,7 @@ public class Piece
     public Piece(string col)
     {
         Color = col;
+
     }
 
 
@@ -17,25 +18,58 @@ public class Piece
 public class Player
 {
     public int playerNumber;
-       
-
+    public string Color {get;set;}
+    public Player(string col)
+    {
+        Color = col;
+    }
 }
 
 public class Board{
-    public Piece[,] playArea = new Piece[6,7];
-
+    public Piece[,] playArea = new Piece[7,6];
     
-
-
-
-
+    public string getStr() 
+    {
+        string boardString = "";
+        foreach ( Piece elm in playArea  )
+        {
+            string tileObj = "" ;
+            if (elm != null)
+            {
+                 tileObj =  elm.Color;
+            }
+            else {tileObj =  "0";}
+            boardString = boardString + $"{tileObj}";
+        }
+        return boardString;
+    }
+            
+    public void move( Player player,int column )
+    {
+        int row = 0;
+        while ( playArea[column,row] != null )
+        {
+            row++;
+        }
+        playArea[column,row ] = new Piece(player.Color ); 
+    }
 }
+
+
+
+
+
 class MainClass
 {
-    public static Main()
+    public static void Main()
     {
         Piece testPiece = new Piece("red");    
+        Board board = new Board();
+        Player elias = new Player("purple");
         Console.WriteLine("I'm alive " + testPiece.Color);
+        Console.WriteLine (board.getStr() );
+        board.move(elias,3);
+        Console.WriteLine (board.getStr() );
     }
 }
 }
