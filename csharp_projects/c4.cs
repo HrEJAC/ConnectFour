a608c12837b40bdbbb010c8ccae1eccb3d33e0d7
Elias Christensen <ejac@me.com>
2018-01-23 11:10:57 +0100

    board.ToString error

--- csharp_projects/c4.cs
+++ csharp_projects/c4.cs
@@ -11,7 +11,10 @@ public class Piece
         Color = col;
 
     }
-
+    public override string ToString()
+    {
+        return (this.Color)[0].ToUpper();
+    }
 
 }
 
@@ -28,22 +31,43 @@ public class Player
 public class Board{
     public Piece[,] playArea = new Piece[7,6];
     
-    public string getStr() 
+    public string getStr(int i, int j) 
     {
-        string boardString = "";
-        foreach ( Piece elm in playArea  )
+        if (this[i,j] != null)
         {
-            string tileObj = "" ;
-            if (elm != null)
-            {
-                 tileObj =  elm.Color;
-            }
-            else {tileObj =  "0";}
-            boardString = boardString + $"{tileObj}";
+            return this[i,j].ToString();
         }
-        return boardString;
+        else {return  "0";}
     }
-            
+   
+    //public override string ToString()
+    //{
+        string str = "";
+        string lineSep = new String('-',7*4);
+        static string BoardStr(int i, int j)
+        {
+            switch ( (i,j) )
+            {
+                case ( (0,0) ):
+                    string str = 
+                        (lineSep+"\n| "+
+                        this.getStr(i,j ) +" ");
+                    return str+boardStr(0,1);
+                case ( (i,6) ):
+                    string str =
+                        ("| "+this.getStr(i,6)+" |\n"+
+                        lineSep+"\n");
+                    return str+getStr(10,10);
+                case ( (10,10) ):
+                    return "";
+                case ( (i,j) ):
+                    string str =
+                        ("| "+this.getStr(i,j)+" ");
+                    return str+boardStr(i,j+1);
+            }
+        }
+    //}
+                
     public void move( Player player,int column )
     {
         int row = 0;
@@ -51,7 +75,8 @@ public class Board{
         {
             row++;
         }
-        playArea[column,row ] = new Piece(player.Color ); 
+    
+    playArea[column,row ] = new Piece(player.Color ); 
     }
 }
 
