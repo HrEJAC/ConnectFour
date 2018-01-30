2465528656cb9a01242e5ee34014c772b7942fc5
Elias Christensen <ejac@me.com>
2018-01-30 14:12:08 +0100

    PC1 can now store games and update weightsheet in 'PC1DATA.txt'

--- csharp_projects/c4.cs
+++ csharp_projects/c4.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace C4
 {
@@ -73,18 +75,160 @@ public class PC0 : Player
 
 public class PC1 : Player
 {
-    public int[] refData;
+    public Double[,] refData = new Double[7,6];
+    
+    //print the sheet in the object
+    public string sData(){
+        string r = "";
+        Double sum = 0.0;
+        foreach (Double i in refData){
+            r = r+i.ToString();
+            sum = sum+i;
+        }
+        r = r+"\n\n"+sum.ToString()+"\n";
+        return r;
+    }    
 
-    public void UpdateData(List<int> data){
-
+    //delete all memory and reset the sheet
+    public void resetData(){
+        try{
+            using (StreamWriter file = 
+                    new StreamWriter("PC1DATA.txt", false))
+            {
+                file.Write("");
+            }
+            
+            using (StreamWriter file = 
+                    new StreamWriter("PC1DATA.txt", true))
+            {
+                string text = "1.0";
+                for (int i = 0;i<=41;i++){
+                    file.WriteLine(text);
+                }
+                file.Close();
+            }
+        }
+        catch(Exception ex){
+            Console.WriteLine("something went wrong:\n"+ex.Message);
+        }
+        for (int i=0;i<=6;i++){
+            for (int j=0;j<=5;j++){
+                refData[i,j]=1.0;
+            }
+        }
     }
 
+    //take finished game as input to update machine's experience
+    public void UpdateData(int[,] data){
+        //input is 1 int for all tiles
+        //0 if empty, 1 if losing piece, 2 if winning piece    
+
+        //save all previous games in RAM
+        List<string> dataTXT = new List<string> {};
+        StreamReader rfile =   
+            new StreamReader("PC1DATA.txt");  
+        //move cursor past the sheet
+        for (int i=1;i<=42;i++){
+            rfile.ReadLine();
+        }
+        //each line represents a played game in memory
+        string line;
+        while ((line=rfile.ReadLine())!=null){
+            dataTXT.Add(line);
+        }
+        rfile.Close();
+        
+        //update datasheet (brain) with input(finished game)
+        //tile rates (winning tile +3%, losing -3%)
+        Double w = 1.03;
+        Double l = 0.97;
+        //go through all tiles and update sheet's (refData's) value
+        for (int i =0;i<=6;i++){
+            for (int j=0;j<=5;j++){
+                Double k = 0.0;
+                switch (data[i,j]){
+                    //if a piece on the winning team is on this tile
+                    case (2):
+                        k = (42.0-refData[i,j]*w)/(42.0-refData[i,j]);
+                        refData[i,j] = refData[i,j]*w;
+                        for (int ii=0;ii<=6;ii++){
+                            for (int jj=0;jj<=5;jj++){
+                                if (!(ii==i&&jj==j)){
+                                    refData[ii,jj]=refData[ii,jj]*k;
+                                }
+                            }
+                        }
+                    break;
+                    //if losing piece is on this tile
+                    case (1):
+                        k = (42.0-refData[i,j]*l)/(42.0-refData[i,j]);
+                        refData[i,j] = refData[i,j]*l;
+                        for (int ii=0;ii<=6;ii++){
+                            for (int jj=0;jj<=5;jj++){
+                                if (!(ii==i&&jj==j)){
+                                    refData[ii,jj]=refData[ii,jj]*k;
+                                }
+                            }
+                        }
+                    break;
+                    //if tile is empty then do nothing
+                    default:
+                    break;
+                }
+            }
+        }
+        //update file with newest sheet
+        using (StreamWriter file = 
+                new StreamWriter("PC1DATA.txt", false))
+        {
+            //collect all info and put it in the datafile
+            for (int i=0;i<=6;i++){
+                for (int j=0;j<=5;j++){
+                    file.WriteLine(refData[i,j].ToString());
+                }
+            }
+            file.Close();
+        }
+        using (StreamWriter file = 
+                new StreamWriter("PC1DATA.txt", true))
+        {
+            line="";
+            for (int i=0;i<=6;i++){
+                for(int j=0;j<=5;j++){
+                    line=line+data[i,j].ToString();
+                }
+            }
+            dataTXT.Add(line);
+            for (int i=0;i<dataTXT.Count;i++){
+                file.WriteLine(dataTXT[i]);
+            }
+            file.Close();
+        }
+    }
+    
+    //take the sheet from memoryfile and store it in the object
     public void GetLatestSheet(){
-
+        string line = "";
+        StreamReader file =   
+            new StreamReader("PC1DATA.txt");  
+        for (int i=0;i<=6;i++){
+            for (int j=0;j<=5;j++){
+                Double num =1.0;
+                line = file.ReadLine();
+                Double.TryParse(line,out num);
+                refData[i,j] = num;
+            }
+        } 
+        file.Close();
     }
 
-    public override NextMove(Board board){
+    public override int NextMove(Board board){
+        return 3;
+    }
 
+    public PC1(string col):base(col)
+    {
+        color = col;
     }
 }
 
@@ -211,8 +355,13 @@ class MainClass
         Board board = new Board();
         Player elias = new Human("red");
         Player asger = new PC0("blue");
-        
-        
+        PC1 els = new PC1("green");
+        els.resetData();
+        Console.WriteLine(els.sData());
+        els.UpdateData(new int[,] {{1,2,9,9,5,6},{9,9,9,9,5,6},{3,3,3,3,5,6},{4,4,4,4,5,6},{9,9,3,4,5,6},{9,9,3,4,5,6},{9,9,3,4,5,6}});
+        els.UpdateData(new int[,] {{1,2,9,9,5,6},{9,9,9,9,5,6},{3,3,3,3,5,6},{4,4,4,4,5,6},{9,9,3,4,5,6},{9,9,3,4,5,6},{9,9,3,4,5,6}});
+        els.GetLatestSheet();
+        Console.WriteLine(els.sData());
         Console.WriteLine("I'm alive " + testPiece.color);
         Console.WriteLine(board.ToString() );
 
@@ -227,7 +376,5 @@ class MainClass
             Console.WriteLine(board.ToString());
             Console.WriteLine(board.CheckWin());
         }
-        
-        Console.WriteLine (board.ToString());
     }
 }}
